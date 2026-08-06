e7fa50060b5ac4f0c1beaa5a37cd8e8cc666fada
Steve <mrpontius@gmail.com>
2026-08-06 17:15:04 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const REPO_NAME = "chronicallyonline";
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
+// const isGithubPages = process.env.GITHUB_PAGES === "true";
+const isGithubPages = "false";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
